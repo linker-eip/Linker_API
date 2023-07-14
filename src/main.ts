bdb03b5099625e06497f3f4ca118862fe4eef6b4
Tony Bano <tony.bano@epitech.eu>
2023-07-14 12:00:46 +0200

    Create profile when register

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
-  await app.listen(process.env.APP_PORT);
+  await app.listen(process.env.APP_PORT || 8080);
 }
 bootstrap();
