aae2646cfbea8179d4f6e22d29d27f6f07ae8923
Tony Bano <tony.bano@epitech.eu>
2023-09-11 17:44:07 +0900

    feat : delete mission

---
+++ src/mission/entity/mission.entity.ts
@@ -0,0 +1,47 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { MissionStatus } from '../enum/mission-status.enum';
+import { CompanyUser } from '../../company/entity/CompanyUser.entity';
+
+@Entity()
+export class Mission {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  name: string;
+
+  @Column({ type: 'enum', enum: MissionStatus, default: 'PENDING' })
+  status: MissionStatus;
+
+  @Column({ type: 'varchar', length: 1024 })
+  description: string;
+
+  @Column({ type: 'int', nullable: true })
+  companyId: number;
+
+  @ManyToOne(() => CompanyUser)
+  @JoinColumn({ name: 'companyId' })
+  company: CompanyUser;
+
+  @Column('simple-array', { nullable: true })
+  studentsIds: number[];
+
+  @Column({ type: 'timestamp' })
+  startOfMission: Date;
+
+  @Column({ type: 'timestamp' })
+  endOfMission: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'float', nullable: false })
+  amount: number;
+}
--- src/student/entity/StudentUser.entity.ts
+++ src/student/entity/StudentUser.entity.ts
@@ -1,39 +1,38 @@
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
-import { Missions } from "../../mission/entity/missions.entity";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Mission } from '../../mission/entity/mission.entity';
 
 @Entity()
-
 export class StudentUser {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({unique: true, nullable: false})
-    email: string;
+  @Column({ unique: true, nullable: false })
+  email: string;
 
-    @Column({nullable: false})
-    password: string;
+  @Column({ nullable: false })
+  password: string;
 
-    @Column({type: "varchar", length: 255, nullable: false})
-    firstName: string;
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  firstName: string;
 
-    @Column({type: "varchar", length: 255, nullable: false})
-    lastName: string;
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  lastName: string;
 
-    @Column({type: "varchar", length: 255, nullable: true})
-    picture: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  picture: string;
 
-    @Column({default: true})
-    isActive: boolean;
+  @Column({ default: true })
+  isActive: boolean;
 
-    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
-    lastConnectedAt: Date
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  lastConnectedAt: Date;
 
-    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
-    createdAt: Date
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
 
-    @Column({ type: 'varchar', nullable: true})
-    resetPasswordToken: string;
+  @Column({ type: 'varchar', nullable: true })
+  resetPasswordToken: string;
 
-    @ManyToMany(() => Missions, missions => missions.students)
-    missions: Missions[];
+  @Column('simple-array', { nullable: true })
+  missionsIds: number[];
 }
