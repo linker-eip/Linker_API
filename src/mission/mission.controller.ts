ce80922618b512e505a9bd0779348e72d6b73300
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-09-15 16:38:27 +0200

    feat: Update mission as a company

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateMissionDto } from './dto/create-mission-dto';
+import { UpdateMissionDto } from './dto/update-mission-dto'
 import { MissionService } from './mission.service';
 import { AuthGuard } from '@nestjs/passport';
 
@@ -27,5 +28,14 @@ export class MissionController {
     })
     async deleteMission(@Param('id') missionId: number, @Req() req) {
         return await this.missionService.deleteMission(missionId, req)
+    }
+
+    @Put(':id')
+    @ApiOperation({
+        description: 'Update a mission as a company',
+        summary: 'Update a mission as a company',
+    })
+    async updateMission(@Param('id') missionId: number, @Body() body: UpdateMissionDto, @Req() req) {
+        return await this.missionService.updateMission(missionId, body, req)
     }
 }
--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -4,6 +4,7 @@ import { Mission } from './entity/mission.entity';
 import { Repository } from 'typeorm';
 import { CreateMissionDto } from './dto/create-mission-dto';
 import { CompanyService } from 'src/company/company.service';
+import { UpdateMissionDto } from './dto/update-mission-dto';
 
 @Injectable()
 export class MissionService {
@@ -57,5 +58,44 @@ export class MissionService {
         }
 
         return this.missionRepository.delete(mission.id)
+    }
+
+    async updateMission(missionId: number, updateMissionDto: UpdateMissionDto, req: any) {
+        let company = null;
+        try {
+            company = await this.companyService.findOne(req.user.email)
+        } catch (err) {
+            throw new HttpException('Invalid company', HttpStatus.UNAUTHORIZED) 
+        }
+
+        const mission = await this.findMissionById(missionId)
+
+        if (mission == null || mission.companyId != company.id) {
+            throw new HttpException('Invalid mission', HttpStatus.NOT_FOUND)
+        }
+
+        const update : Partial<Mission> = {};
+        if (updateMissionDto.name !== null) {
+        update.name = updateMissionDto.name;
+        }
+
+        if (updateMissionDto.description !== null) {
+        update.description = updateMissionDto.description;
+        }
+
+        if (updateMissionDto.startOfMission !== null) {
+        update.startOfMission = updateMissionDto.startOfMission;
+        }
+
+        if (updateMissionDto.endOfMission !== null) {
+        update.endOfMission = updateMissionDto.endOfMission;
+        }
+
+        if (updateMissionDto.amount !== null) {
+        update.amount = updateMissionDto.amount;
+        }
+
+        await this.missionRepository.update(mission.id, update);
+        return await this.findMissionById(missionId)
     }
 }
