fddf87ff222e2a50ab61f320a4a6fb95820ca160
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-09-15 16:11:11 +0200

    feat: Delete mission as a company

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateMissionDto } from './dto/create-mission-dto';
 import { MissionService } from './mission.service';
@@ -6,7 +6,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 @ApiBearerAuth()
 @UseGuards(AuthGuard('jwt'))
-@ApiTags('/mission')
+@ApiTags('Mission')
 @Controller('mission')
 export class MissionController {
     constructor(private readonly missionService: MissionService) {}
@@ -18,5 +18,14 @@ export class MissionController {
     })
     async createMission(@Req() req, @Body() body: CreateMissionDto) {
         return await this.missionService.createMission(body, req);
+    }
+
+    @Delete(':id')
+    @ApiOperation({
+        description: 'Delete a mission as a company',
+        summary: 'Delete a mission as a company',
+    })
+    async deleteMission(@Param('id') missionId: number, @Req() req) {
+        return await this.missionService.deleteMission(missionId, req)
     }
 }
--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -7,15 +7,23 @@ import { CompanyService } from 'src/company/company.service';
 
 @Injectable()
 export class MissionService {
+
     constructor(
         @InjectRepository(Mission)
         private readonly missionRepository: Repository<Mission>,
         private readonly companyService: CompanyService
     ) {}
 
+    async findMissionById(missionId: number) : Promise<Mission> {
+        const mission = await this.missionRepository.findOne({
+          where: { id: missionId },
+        });
+
+        return mission;
+      }
+
     async createMission(createMissionDto: CreateMissionDto, req: any) {
         let company = null;
-        console.log(req)
         try {
             company = await this.companyService.findOne(req.user.email)
         } catch (err) {
@@ -32,5 +40,22 @@ export class MissionService {
         mission.companyId = company.id;
 
         return await this.missionRepository.save(mission);
+    }
+
+    async deleteMission(missionId: number, req: any) {
+        let company = null;
+        try {
+            company = await this.companyService.findOne(req.user.email)
+        } catch (err) {
+            throw new HttpException('Invalid company', HttpStatus.UNAUTHORIZED) 
+        }
+
+        const mission = await this.findMissionById(missionId)
+
+        if (mission == null || mission.companyId != company.id) {
+            throw new HttpException('Invalid mission', HttpStatus.NOT_FOUND)
+        }
+
+        return this.missionRepository.delete(mission.id)
     }
 }
