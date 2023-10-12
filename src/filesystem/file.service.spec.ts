39ba208d3e85652c5afe5d88ddd8be79e001ad07
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-10-12 13:47:58 +0200

    test: FileSystem

--- src/filesystem/file.service.spec.ts
+++ src/filesystem/file.service.spec.ts
@@ -20,9 +20,9 @@ describe('FileService', () => {
       controllers: [FileController],
       providers: [FileService],
     })
-    .overrideGuard(AuthGuard('jwt'))
-    .useValue({ canActivate: jest.fn(() => true) })
-    .compile();
+      .overrideGuard(AuthGuard('jwt'))
+      .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
 
     controller = module.get<FileController>(FileController);
     service = module.get<FileService>(FileService);
@@ -30,12 +30,38 @@ describe('FileService', () => {
 
   describe('getFile', () => {
     it('should return a file object', async () => {
-        jest.spyOn(service, 'getFile').mockReturnValueOnce(null)
+      jest.spyOn(service, 'getFile').mockReturnValueOnce(null)
 
-        const response = await controller.getFile("fileName", null)
-        expect(response).toEqual(null);
+      const response = await controller.getFile("fileName", null)
+      expect(response).toEqual(null);
     })
   })
+
+  describe('uploadFile', () => {
+    it('should upload a file', async () => {
+      const file: Express.Multer.File = {
+        fieldname: 'file',
+        originalname: 'test-file.txt',
+        encoding: '7bit',
+        mimetype: 'text/plain',
+        destination: './uploads',
+        filename: 'test-file.txt',
+        path: './uploads/test-file.txt',
+        size: 12345,
+        stream: null,
+        buffer: Buffer.from(''),
+
+      };
+
+      jest.spyOn(service, 'storeFile').mockReturnValueOnce(Promise.resolve("linker-external/public/test-file.txt"));
+
+      const response = await controller.uploadFile(file)
+
+      expect(response).toEqual('linker-external/public/test-file.txt');
+      expect(service.storeFile).toHaveBeenCalledWith(file);
+    })
+  });
+
 
   it('should be defined', () => {
     expect(service).toBeDefined();
