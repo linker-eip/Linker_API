91e7acd2ca9d7837d78df50fa3327d254cdd8d0c
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-10-15 14:29:15 +0200

    Changed coverage check script

--- check-coverage.js
+++ check-coverage.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 const coverageReport = JSON.parse(fs.readFileSync('coverage/coverage-summary.json', 'utf8'));
 
 // Extraire le pourcentage de "branch coverage" du rapport
-const branchCoverage = coverageReport.total.branches.pct;
-console.log(`Branch coverage: ${branchCoverage}%`);
+const branchCoverage = coverageReport.total.statements.pct
+console.log(`Statements coverage: ${branchCoverage}%`);
 
 // Vérifier que le "branch coverage" est d'au moins 80%
-if (branchCoverage < 80) {
-    console.error('Branch coverage is below 80%');
+if (branchCoverage < 90) {
+    console.error('Statements coverage is below 80%');
     process.exit(1);
 }
