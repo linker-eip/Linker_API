93200e03a551b05dc7c007c09d6c61cc162ec410
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-12-17 18:13:37 +0100

    fix : logs

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -242,6 +242,7 @@ export class GroupService {
 
         let groups = Promise.all(groupInvites.map(async it => {
             let group = await this.groupRepository.findOne({where: {id: it.groupId}})
+            console.log(group)
             console.log(group.leaderId)
             let leader = await this.studentService.findOneById(group.leaderId)
             let response : GetInvitesResponse = {id: group.id, name: group.name, picture: group.picture, leaderName: leader.firstName + ' ' + leader.lastName}
