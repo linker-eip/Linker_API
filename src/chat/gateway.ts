5c63d6c42aa709b2af122cbb1670b9aab1eb22eb
Jérémy Calvo <jeremy.calvo@epitech.eu>
2024-01-15 11:12:47 +0100

    feat: More on message

--- src/chat/gateway.ts
+++ src/chat/gateway.ts
@@ -6,6 +6,7 @@ import { HttpStatusCode } from 'axios'
 import { Server, Socket } from 'socket.io'
 import { StudentUser } from 'src/student/entity/StudentUser.entity'
 import { StudentService } from 'src/student/student.service'
+import { StudiesDto } from 'src/student/studies/dto/studies.dto'
 
 @WebSocketGateway()
 export class Gateway implements OnModuleInit {
@@ -47,10 +48,17 @@ export class Gateway implements OnModuleInit {
 
     @SubscribeMessage('sendGroup')
     onNewMessage(@MessageBody() body: any, @ConnectedSocket() socket: Socket) {
-        const studentUser = this.studentUsers[socket.id]
+        const studentUser: StudentUser = this.studentUsers[socket.id]
         if (studentUser == null) {
             socket.emit('error', { message: 'Unauthorized access' });
         }
-        this.server.to("GROUP_" + studentUser.groupId).emit("groupMessage", body.message)
+        let message = {
+            content: body.message,
+            firstName: studentUser.firstName,
+            lastName: studentUser.lastName,
+            picture: studentUser.picture,
+        }
+        this.server.to("GROUP_" + studentUser.groupId).emit("groupMessage", message)
+
     }
 }
