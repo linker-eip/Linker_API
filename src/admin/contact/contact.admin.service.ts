ffe657d46c54d5707d000425c9c9182b6539c110
Tony Bano <tony.bano@epitech.eu>
2024-02-19 13:25:20 +0100

    feat : admin get contact and update

---
+++ src/admin/contact/contact.admin.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Contact } from './entity/contact.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UpdateContactDto } from './dto/update-contact.dto';
+
+@Injectable()
+export class ContactAdminService {
+  constructor(
+    @InjectRepository(Contact)
+    private readonly contactRepository: Repository<Contact>,
+  ) {}
+
+  async findAll(): Promise<Contact[]> {
+    return await this.contactRepository.find();
+  }
+
+  async update(
+    id: number,
+    updateContactDto: UpdateContactDto,
+  ): Promise<Contact> {
+    const contact = await this.contactRepository.findOne({ where: { id: id } });
+
+    if (!contact) {
+      throw new NotFoundException(`CONTACT_NOT_FOUND`);
+    }
+
+    contact.isTreated = updateContactDto.isTreated;
+    return await this.contactRepository.save(contact);
+  }
+}
---
+++ src/admin/contact/dto/update-contact.dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsOptional } from "class-validator";
+
+
+export class UpdateContactDto {
+    @ApiProperty()
+    @IsOptional()
+    isTreated?: boolean;
+}
